038401bccd6ac364c6a6a4dc01046e6f65885136
Daniel Muñiz Fontoira <dani@damufo.com>
2017-07-27 00:41:36 +0200

    Fix to work with API-1.0

--- panel-plugin/xfcetimer.c
+++ panel-plugin/xfcetimer.c
@@ -38,7 +38,8 @@
 #include <glib/gprintf.h>  // for gcc's warning: implicit declaration of function 'g_sprintf'
 #include <libxfce4util/libxfce4util.h>
 #include <libxfce4ui/libxfce4ui.h>
-#include <libxfce4panel/xfce-panel-plugin.h>
+/*#include <libxfce4panel/xfce-panel-plugin.h>*/
+#include <libxfce4panel/libxfce4panel.h>
 
 #include "xfcetimer.h"
 
